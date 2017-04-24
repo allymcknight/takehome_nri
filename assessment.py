a87baf6be65ace39c24463a07f7e57285bec43cc
Allyson McKnight <allyson.tong10@gmail.com>
2017-04-24 10:50:13 -0700

    added assessment.py and successfully grabbed bash input

---
+++ assessment.py
@@ -0,0 +1,13 @@
+"""
+This is a python file created for the NoRedInk take home challenge.
+It should take in a int argument through the command line and return
+a list of comma-separated question ids based off of even distribution
+between all three categories of strands<standards<questions. 
+
+"""
+import sys
+
+
+NUM_QUESTIONS = int(sys.argv[1])
+
+
